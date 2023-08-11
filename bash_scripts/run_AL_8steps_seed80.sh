2618567f80168c22e4fcdf0f58d3878628b06be3
Drew Prinster <46874730+drewprinster@users.noreply.github.com>
2023-08-11 11:01:30 -1000

    Bash scripts for executing expts on HPC clusters

---
+++ bash_scripts/run_AL_8steps_seed80.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#SBATCH -t 1- 
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=4
+#SBATCH --mem=20G
+python ../run_JAWS-X_active.py --dataset airfoil --n_steps 8 --n_queries_ann 16 --n_train_initial 32 --seed_initial 80 --n_seed 1 --K_vals 16
---
+++ bash_scripts/run_JAWS-X_red_n192_lam0.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#SBATCH -t 2- 
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=10
+#SBATCH --mem=50G 
+python ../run_JAWS-X_FCS_expts.py --fitness_str red --n_trains 192 --lmbdas 0 --n_seed 20 --K_vals 8 12 16 24 32 48 96 192
